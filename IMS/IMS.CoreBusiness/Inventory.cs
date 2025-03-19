f393ed677d5a89f1fa8f7b514c9b9994010c2936
rdomingo <randolph.domingo@gmail.com>
2025-03-20 09:56:11 +1300

    Blazor IMS

---
+++ IMS/IMS.CoreBusiness/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        public string InventoryName { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
